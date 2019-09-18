7b01216049c819acd3daa83904af4bf3b3bff7ab
forwardvibe <admin@forwardvibe.com>
2019-09-18 13:33:09 -0400

    Update euler35.py

--- euler35.py
+++ euler35.py
@@ -4,9 +4,11 @@
 from math import sqrt
 from itertools import count, islice
 
+
 # Check if number is prime
 def isPrime(n):
 	return n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
+
 	
 # Variable to store all sets of rotational primes
 primes = []
